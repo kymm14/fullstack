7ba4803bcc1db13517d17cb8acfc0981a6c50807
Yone <yonetzymm@gmail.com>
2023-02-06 13:33:58 +0100

    actualizacion

---
+++ JAVASCRIPT/primer.js
@@ -0,0 +1,6 @@
+alert("Hello world!");
+document.write("Hello world!");
+console.log('en', 'un', 'lugar', 'de', 'la', 'Mancha');
+console.info('Info');
+console.warn('Warning');
+console.error('Error');
